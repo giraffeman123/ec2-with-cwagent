4ae8a1cf64ccd13579b86607d4e5ff813b377af8
eduardo elliot martinez palomino <elliotmtz12@gmail.com>
2024-10-15 12:02:52 -0700

    updated apache tomcat download link and fix bootstrap issue with user-data

--- modules/ec2/scripts/ec2-init-script.sh
+++ modules/ec2/scripts/ec2-init-script.sh
@@ -5,6 +5,7 @@ sudo apt-get update
 
 #install unzip package
 sudo apt-get -y install unzip
+sudo apt-get -y install curl
 
 #install java 17
 sudo apt-get -y install openjdk-17-jdk openjdk-17-jre
@@ -15,13 +16,16 @@ java -version
 sudo apt-get -y install maven
 mvn -version
 
-#install and configure tomcat 10
+#install and configure tomcat 10.1.25
 sudo mkdir /opt/tomcat
 cd /tmp
-wget [ADD_HERE_URL_TO_APACHE_TOMCAT]
+#you can choose either the binary found in repository or validate if the link to apache site is still working...
+curl https://raw.githubusercontent.com/giraffeman123/ec2-with-cwagent/java-spring/tomcat-binary/apache-tomcat-10.1.25.zip --output apache-tomcat-10.1.25.zip
 sudo unzip apache-tomcat-10.1.25.zip
 sudo mv apache-tomcat-10.1.25/* /opt/tomcat
-#sudo tar xzvf apache-tomcat-10*tar.gz -C /opt/tomcat --strip-components=1
+
+# wget https://archive.apache.org/dist/tomcat/tomcat-10/v10.1.25/bin/apache-tomcat-10.1.25.tar.gz
+# sudo tar xzvf apache-tomcat-10*tar.gz -C /opt/tomcat --strip-components=1
 
 sudo chown -R ubuntu:ubuntu /opt/tomcat/
 sudo chmod -R u+x /opt/tomcat/bin
@@ -85,6 +89,7 @@ cd tech-interview-xaldigital/web-app/
 sudo mvn clean package
 sudo chown -R ubuntu:ubuntu target/WebApp.war
 sudo mv /home/ubuntu/tech-interview-xaldigital/web-app/target/WebApp.war /opt/tomcat/webapps/ROOT.war
+sudo rm -rf /opt/tomcat/webapps/ROOT/
 
 sudo systemctl restart tomcat
 
